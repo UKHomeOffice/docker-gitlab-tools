fd72cc9626f152e7dde0bb8356423da922be4def
Rohith <gambol99@gmail.com>
2015-09-14 21:42:13 +0100

    - passing the command line arguments to the options - fixed up the command line path

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-SCHEDULE=${SCHEDULE:-""}
 GO_CRON=${GO_CRON:-/go-cron}
-$GO_CRON "${SCHEDULE}" bin/bash -c "/gitlab-alert.rb"
+$GO_CRON "${SCHEDULE}" /bin/bash -c "/bin/gitlab-alert.rb $@"
